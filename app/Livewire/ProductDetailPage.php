6d44886c777323cf57755343a2f478d6b0b1adf4
Ivan Cortes <ivancortes027@gmail.com>
2025-11-21 22:21:28 +0800

    14.E-Commerce Project

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -1,13 +1,22 @@
 <?php
 
 namespace App\Livewire;
-
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('Product Detail - LAZZAAPPEE')]
 class ProductDetailPage extends Component
 {
+    public $slug;
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+    }
     public function render()
     {
-        return view('livewire.product-detail-page');
+        return view('livewire.product-detail-page',[
+        'product'=>Product::where('slug',$this->slug)->firstOrFail(),
+        ]);
     }
 }
