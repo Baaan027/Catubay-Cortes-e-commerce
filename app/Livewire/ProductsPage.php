5ab264e86352149435583c1bd1ca7a6f1ac9212c
Ivan Cortes <ivancortes027@gmail.com>
2025-11-22 00:02:22 +0800

    16.E-Commerce Project

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -15,8 +15,10 @@ class ProductsPage extends Component
     use WithPagination;
     #[Url]
     public $selected_categories = [];
+
     #[Url]
     public $selected_brands = [];
+
     #[Url] 
     public $featured;   
 
@@ -24,6 +26,10 @@ class ProductsPage extends Component
     public $on_sale;
     #[Url]
     public $price_range = 300000;
+
+    #[Url]
+    public $sort = 'latest';
+
     public function render()
     {
         $productQuery = Product::query()->where('is_active', 1);
@@ -42,6 +48,12 @@ class ProductsPage extends Component
         }
         if($this->price_range){
             $productQuery->whereBetween('price', [0, $this->price_range]);
+        }
+        if($this->sort ==='latest'){
+            $productQuery->latest();
+        }
+        if($this->sort =='price'){
+            $productQuery->orderBy('price');
         }
         return view('livewire.products-page',
         ['products' => $productQuery->paginate(9),
